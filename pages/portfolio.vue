e4fe9edd1b806bda19d7209074409eb3355f2910
Cory Lashway <cory.lashway@gmail.comc>
2018-07-04 16:24:20 -0400

    Remove overloading styling

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -24,52 +24,5 @@ export default {
 </script>
 
 <style>
-.container {
-  display: grid;
-  grid-template-columns: 10px 300px 50px;
-  grid-template-rows: 10px 1fr 550px 60px 30px;
-  grid-template-areas:
-    ". . ."
-    ". header ."
-    ". main-content ."
-    ". footer ."
-    ". . .";
-  grid-gap: 30px;
-  min-height: 100vh;
-}
-.header {
-  background: #fff;
-  grid-area: header;
-}
-.nav {
-  background: #fff;
-  grid-area: nav;
-}
-.main-content {
-  padding-left: 10px;
-  background: #fff;
-  grid-area: main-content;
-}
-.footer {
-  background: #1e1e1e;
-  text-align: center;
-  line-height: 55px;
-  color: white;
-  grid-area: footer;
-}
-@media screen and (min-width: 800px) {
-  .container {
-    display: grid;
-    grid-template-columns: 10px 300px 1fr 50px;
-    grid-template-rows: 10px 60px 500px 60px 30px;
-    grid-template-areas:
-      ". . . ."
-      ". header nav ."
-      ". header main-content ."
-      ". header footer ."
-      ". . . .";
-    grid-gap: 30px;
-    min-height: 100vh;
-  }
-}
+
 </style>
